2c09a05ab6e44490837530fd7b59e87a676bb187
mikita.papok <papokmikita@gmail.com>
2022-07-31 13:49:43 +0300

    commit n-2

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-const burgerMenuButton=document.querySelector('.burger-menu')
-const sidebar=document.querySelector('.sidebar')
-const sheet=document.querySelector('.sheet')
-const closeSideBarButton=document.querySelector('.close-sidebar')
-const sidebarNavigator=document.querySelector('.sidebar-navigator')
+const burgerMenuButton = document.querySelector(".burger-menu");
+const sidebar = document.querySelector(".sidebar");
+const sheet = document.querySelector(".sheet");
+const closeSideBarButton = document.querySelector(".close-sidebar");
+const sidebarNavigator = document.querySelector(".sidebar-navigator");
 
-const closeSideBar=()=>{
-    sidebar.classList.add('sidebar-display')
-    sheet.classList.add('sheet-no-display')
-
-}
-const openSideBar=()=>{
-    sheet.classList.remove('sheet-no-display')
-    sidebar.classList.remove('sidebar-display')
-}
-burgerMenuButton.addEventListener('click',openSideBar)
-closeSideBarButton.addEventListener('click',closeSideBar)
+const closeSideBar = () => {
+    sidebar.classList.add("sidebar-display");
+    sheet.classList.add("sheet-no-display");
+};
+const openSideBar = () => {
+    sheet.classList.remove("sheet-no-display");
+    sidebar.classList.remove("sidebar-display");
+};
+burgerMenuButton.addEventListener("click", openSideBar);
+closeSideBarButton.addEventListener("click", closeSideBar);
